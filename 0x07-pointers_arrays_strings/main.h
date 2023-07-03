e111853b328a3475f7b7a1a9c21e87bd68788127
miirshe <miirshe@gmail.com>
2023-07-03 10:48:08 -0700

    Last Upadate

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
-main.h
-
-#ifndef MAIN_H
-#define MAIN_H
+i#ifndef main.h
+#define main.h
 
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
@@ -13,4 +11,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-#endif
